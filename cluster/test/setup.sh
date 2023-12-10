f665b2fb651733db422091f4d1dd5acff5a3623d
Ben Lipton <ben.lipton@gmail.com>
2023-12-10 15:29:41 -0500

    Fix apiVersions

--- cluster/test/setup.sh
+++ cluster/test/setup.sh
@@ -13,7 +13,7 @@ ${KUBECTL} -n upbound-system wait --for=condition=Available deployment --all --t
 
 echo "Creating a default provider config..."
 cat <<EOF | ${KUBECTL} apply -f -
-apiVersion: snowflake.upbound.io/v1beta1
+apiVersion: snowflake.benjaminlipton.com/v1alpha1
 kind: ProviderConfig
 metadata:
   name: default
