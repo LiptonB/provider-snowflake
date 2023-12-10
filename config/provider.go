cd9b469f3ecb2f13766d3459e8184b88726c77d6
Ben Lipton <ben.lipton@gmail.com>
2023-12-10 14:46:52 -0500

    Attempt to add and register custom config

--- config/provider.go
+++ config/provider.go
@@ -10,7 +10,7 @@ import (
 
 	ujconfig "github.com/crossplane/upjet/pkg/config"
 
-	"github.com/LiptonB/provider-snowflake/config/null"
+	"github.com/LiptonB/provider-snowflake/config/warehouse"
 )
 
 const (
@@ -36,7 +36,7 @@ func GetProvider() *ujconfig.Provider {
 
 	for _, configure := range []func(provider *ujconfig.Provider){
 		// add custom config functions
-		null.Configure,
+		warehouse.Configure,
 	} {
 		configure(pc)
 	}
