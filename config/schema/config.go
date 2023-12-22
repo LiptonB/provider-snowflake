d3e586668c6a040dbb8ec84c253a3043d1d1ce3e
Ben Lipton <ben.lipton@gmail.com>
2023-12-22 10:59:14 -0500

    Attempt to add a reference

--- config/schema/config.go
+++ config/schema/config.go
@@ -8,5 +8,10 @@ func Configure(p *config.Provider) {
 		// We need to override the default group that upjet generated for
 		// this resource, which would be "snowflake"
 		// r.ShortGroup = "warehouse"
+		r.References = config.References {
+			"database": {
+				Type: "github.com/LiptonB/provider-snowflake/apis/snowflake/v1alpha1.Database"
+			}
+		}
 	})
 }
