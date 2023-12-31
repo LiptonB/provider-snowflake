740c4ce9243308c49e446179a50213e3e7428779
Ben Lipton <ben.lipton@gmail.com>
2023-12-31 11:23:06 -0500

    Removed unnecessary comments from configs

--- config/database/config.go
+++ config/database/config.go
@@ -5,8 +5,5 @@ import "github.com/crossplane/upjet/pkg/config"
 // Configure configures individual resources by adding custom ResourceConfigurators.
 func Configure(p *config.Provider) {
 	p.AddResourceConfigurator("snowflake_database", func(r *config.Resource) {
-		// We need to override the default group that upjet generated for
-		// this resource, which would be "snowflake"
-		// r.ShortGroup = "warehouse"
 	})
 }
--- config/view/config.go
+++ config/view/config.go
@@ -5,9 +5,6 @@ import "github.com/crossplane/upjet/pkg/config"
 // Configure configures individual resources by adding custom ResourceConfigurators.
 func Configure(p *config.Provider) {
 	p.AddResourceConfigurator("snowflake_view", func(r *config.Resource) {
-		// We need to override the default group that upjet generated for
-		// this resource, which would be "snowflake"
-		// r.ShortGroup = "warehouse"
 		r.References = config.References{
 			"database": {
 				Type: "github.com/LiptonB/provider-snowflake/apis/snowflake/v1alpha1.Database",
