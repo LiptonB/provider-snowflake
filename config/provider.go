8dcd39b4c14234a4284327ba4ef5bcd41655d495
Ben Lipton <ben.lipton@gmail.com>
2024-01-14 10:32:57 -0500

    Seems like maybe there is meant to be another layer of API group nesting

--- config/provider.go
+++ config/provider.go
@@ -34,7 +34,7 @@ var providerMetadata string
 // GetProvider returns provider configuration
 func GetProvider() *ujconfig.Provider {
 	pc := ujconfig.NewProvider([]byte(providerSchema), resourcePrefix, modulePath, []byte(providerMetadata),
-		ujconfig.WithRootGroup("crossplane.io"),
+		ujconfig.WithRootGroup("snowflake.crossplane.io"),
 		ujconfig.WithIncludeList(ExternalNameConfigured()),
 		ujconfig.WithFeaturesPackage("internal/features"),
 		ujconfig.WithDefaultResourceOptions(
